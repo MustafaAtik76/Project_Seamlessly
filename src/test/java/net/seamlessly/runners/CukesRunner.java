822b52cf47c2021e22253cb94631e3c39731e18b
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-14 00:28:33 +0300

    login.features updated

--- src/test/java/net/seamlessly/runners/CukesRunner.java
+++ src/test/java/net/seamlessly/runners/CukesRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 //"pretty",//prints each step's detail on the console
+                "json:target/cucumber.json",
                 "html:target/cucumber_report.html",//html is the type of the report and the report is saved under the target folder and not pushed to gitHub
                 "rerun:target/rerun.txt",//reports the failed tests and shows the lines of the related class and coding.
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",//Maven cucumber Reporting ( pretty ). Creates a comprehensive demonstrative report on a web page
@@ -15,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",//links to the "features" packagee
         glue = "src/test/step_definitions",//Links to the "step_definitions" package
         dryRun = true,
-        tags = "@wip"
+        tags = ""
        // publish = true//creates a link of web browser on the console that includes a report so that the report page can be shared
 
 
--- src/test/java/net/seamlessly/step_definitions/LoginStepDefinitions.java
+++ src/test/java/net/seamlessly/step_definitions/LoginStepDefinitions.java
@@ -1,6 +1,36 @@
 package net.seamlessly.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.seamlessly.pages.LoginPage;
+import net.seamlessly.utilities.BrowserUtils;
+import net.seamlessly.utilities.Driver;
+
 public class LoginStepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        Driver.getDriver().get("seamlesslyUrl");
+    }
+    @When("user enters {string} as username")
+    public void user_enters_as_username(String userName) {
+        loginPage.userName.sendKeys(userName);
+    }
+    @When("user enters {string} as password")
+    public void user_enters_as_password(String password) {
+        loginPage.password.sendKeys(password);
+    }
+    @When("user clicks login button")
+    public void user_clicks_login_button() {
+        loginPage.loginButton.click();
+    }
+    @Then("user should login successfully")
+    public void user_should_login_successfully() {
+        BrowserUtils.verifyURLContains("files");
+    }
 
 
 }
