f381b66d279823e96690361ea6dfc193fdff8c23
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-18 01:43:27 +0300

    LogOutPage created

---
+++ src/test/java/net/seamlessly/pages/LogOutPage.java
@@ -0,0 +1,30 @@
+package net.seamlessly.pages;
+
+import net.seamlessly.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogOutPage {
+    public LogOutPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "expand")
+    public WebElement avatarIcon;
+
+    @FindBy(xpath = "//li[@data-id='logout']")
+    public WebElement logoutLink;
+
+    @FindBy(id = "user")
+    public WebElement userName;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+
+
+}
